b7f3d4e1fa1725dffe35f2983acb28b7b95406bc
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-07 13:33:23 +0100

    add config for babel

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest"
   },
   transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
 };
