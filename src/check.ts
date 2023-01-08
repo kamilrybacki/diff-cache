594b9246c0e48906bdc5768fb5704a4d2629fe11
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 18:40:49 +0100

    fix diff command

--- src/check.ts
+++ src/check.ts
@@ -1,8 +1,8 @@
 import { execSync } from 'child_process';
 
 const check = async (include: string, exclude: string): Promise<string> => {
-  console.log(`Checking staged files for ${include} and excluding ${exclude}`);
-  const diffCommand = `git diff HEAD^ HEAD --cached --name-only --diff-filter=ACMRTUXB`;
+  console.log(`Checking staged files for ${include} ${exclude ? `and excluding ${exclude}` : ''}}`);
+  const diffCommand = `git diff HEAD^ HEAD --diff-filter="ACMRTUXB"`;
   const findFilesCommand = `${diffCommand} | grep -E "${include}" | grep -vE "${exclude}"`;
   const files = execSync(findFilesCommand).toString();
   console.log(files)
