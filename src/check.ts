fe91e0323bdc3c0b9b570a3aadf6097c807ef768
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 18:26:40 +0100

    add console log

--- src/check.ts
+++ src/check.ts
@@ -1,6 +1,9 @@
+import { execSync } from 'child_process';
 
 const check = async (include: string, exclude: string): Promise<string> => {
   console.log(`Checking staged files for ${include} and excluding ${exclude}`);
+  const files = execSync(`git diff HEAD^ HEAD`).toString();
+  console.log(files)
   return '';
 };
 
