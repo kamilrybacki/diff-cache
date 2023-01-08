25f36604c4f457da5200cf65c950f8b2d5b942b3
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 18:31:48 +0100

    add correct commits to diff

--- src/check.ts
+++ src/check.ts
@@ -2,7 +2,8 @@ import { execSync } from 'child_process';
 
 const check = async (include: string, exclude: string): Promise<string> => {
   console.log(`Checking staged files for ${include} and excluding ${exclude}`);
-  const files = execSync(`git diff HEAD^ HEAD`).toString();
+  const diffCommand = `git diff HEAD HEAD~1 --cached --name-only --diff-filter=ACMRTUXB | grep -E "${include}" | grep -vE "${exclude}"`;
+  const files = execSync(diffCommand).toString();
   console.log(files)
   return '';
 };
