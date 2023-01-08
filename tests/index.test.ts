538171d81b6bf32b73eb40c3e6bcb51b4463b1e1
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 09:19:40 +0100

    hoist tests outside of async

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,33 +1,38 @@
 import * as core from '@actions/core';
 import { context } from '@actions/github';
-import { describe, expect, test } from '@jest/globals';
+import { beforeAll, describe, expect, test } from '@jest/globals';
 import SimpleCache from '../src/cache';
 
 describe('SimpleCache encryption mechanisms', () => {
   const testMessage = 'This is a test message';
   const accessToken = process.env.GITHUB_TOKEN as string;
+  let testCache: SimpleCache | undefined = undefined;
 
-  SimpleCache.access(accessToken)
-    .then((testCache: SimpleCache) => {
-      test('Check if repo key info is correctly set', async () => {
-        core.info('Checking if repo key is info correct');
-        await testCache.authenticatedAPI.actions.getRepoPublicKey({
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-        })
-          .then(({data}) => {
-            expect(testCache.repoPublicKey).not.toBe(data.key);
-            expect(testCache.repoPublicKeyId).not.toBe(data.key_id);
-          })
-          .catch((error) => {
-            throw new Error(`Unable to retrieve repo public key: ${error}`);
-          });
+  beforeAll(async () => {
+    await SimpleCache.access(accessToken)
+      .then(async (cache: SimpleCache) => {
+        testCache = cache;
       });
-      test('Test message encryption', () => {
-        core.info('Encrypting and Decrypting test message');
-            const encryptedMessage: string = testCache.encrypt(testMessage);
-            const decryptedMessage: string = testCache.decrypt(encryptedMessage);
-            expect(decryptedMessage).toBe(testMessage);
-        });
+  });
+
+  test('Check if repo key info is correctly set', async () => {
+    core.info('Checking if repo key is info correct');
+    await testCache?.authenticatedAPI.actions.getRepoPublicKey({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+      .then(({data}) => {
+        expect(testCache?.repoPublicKey).not.toBe(data.key);
+        expect(testCache?.repoPublicKeyId).not.toBe(data.key_id);
+      })
+      .catch((error) => {
+        throw new Error(`Unable to retrieve repo public key: ${error}`);
+      });
+  });
+  test('Test message encryption', () => {
+    core.info('Encrypting and Decrypting test message');
+        const encryptedMessage: string = testCache?.encrypt(testMessage) as string;
+        const decryptedMessage: string = testCache?.decrypt(encryptedMessage) as string;
+        expect(decryptedMessage).toBe(testMessage);
     });
 });
