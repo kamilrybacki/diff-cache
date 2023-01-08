7ca22d17fd373b2ffe030c50de56a78b19461d62
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 18:37:53 +0100

    fix checkout

--- jest.config.ts
+++
@@ -1,4 +0,0 @@
-import type { Config } from '@jest/types'
-
-const config: Config.InitialOptions = 
-export default config;
--- src/check.ts
+++ src/check.ts
@@ -2,8 +2,9 @@ import { execSync } from 'child_process';
 
 const check = async (include: string, exclude: string): Promise<string> => {
   console.log(`Checking staged files for ${include} and excluding ${exclude}`);
-  const diffCommand = `git diff HEAD HEAD~1 --cached --name-only --diff-filter=ACMRTUXB | grep -E "${include}" | grep -vE "${exclude}"`;
-  const files = execSync(diffCommand).toString();
+  const diffCommand = `git diff HEAD^ HEAD --cached --name-only --diff-filter=ACMRTUXB`;
+  const findFilesCommand = `${diffCommand} | grep -E "${include}" | grep -vE "${exclude}"`;
+  const files = execSync(findFilesCommand).toString();
   console.log(files)
   return '';
 };
