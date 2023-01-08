cd91b06be2dcecf9de166457eca9c188930ae0b6
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 18:55:09 +0100

    promisify diff

--- src/check.ts
+++ src/check.ts
@@ -1,12 +1,20 @@
+import * as actionsConsole from './actionsConsole.js';
 import { execSync } from 'child_process';
 
-const check = async (include: string, exclude: string): Promise<string> => {
-  console.log(`Checking staged files for ${include} ${exclude ? `and excluding ${exclude}` : ''}}`);
+const diff = async (include: string, exclude: string): Promise<string> => {
   const diffCommand = `git diff HEAD^ HEAD --diff-filter="ACMRTUXB"`;
   const findFilesCommand = `${diffCommand} | grep -E "${include}" | grep -vE "${exclude}"`;
-  const files = execSync(findFilesCommand).toString();
-  console.log(files)
-  return '';
+  return new Promise<string>(() => execSync(findFilesCommand, { encoding: 'utf8' }));
+};
+
+const check = async (include: string, exclude: string): Promise<string> => {
+  console.log(`Checking staged files for ${include} ${exclude ? `and excluding ${exclude}` : ''}`);
+  return await diff(include, exclude)
+    .then((files) => {
+      actionsConsole.info(files)
+      return files;
+    })
+
 };
 
 export default check;
