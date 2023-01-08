be930c4a70d195f8dacfbac3b2c36ee88c62afd9
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-08 13:35:37 +0100

    print envs

--- index.ts
+++ index.ts
@@ -1,33 +1,28 @@
+import * as actionsConsole from './src/actionsConsole';
 import * as core from '@actions/core';
 import check from './src/check.js';
 import update from './src/update.js';
 import SimpleCache from './src/cache.js';
 
 async function run() {
-  try {
-    const include = core.getInput('include');
-    core.info(`Using regex: ${include}`);
+  const include = core.getInput('include');
+  core.info(`Using regex: ${include}`);
 
-    const exclude = core.getInput('exclude');
-    if (exclude) core.info(`Using ignore: ${exclude}`);
+  const exclude = core.getInput('exclude');
+  if (exclude) core.info(`Using ignore: ${exclude}`);
 
-    const token = core.getInput('token');
-    await SimpleCache.access(token).then(async () => {
+  actionsConsole.info(process.env)
+
+  const token = core.getInput('token');
+  await SimpleCache
+    .access(token)
+    .then(async () => {
       await check(include, exclude)
         .then(async (stagedFiles: string) => {
           stagedFiles != '' ? await update(stagedFiles) : core.info('No staged files to lint');
-      });
-    });
-
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      core.setFailed(error.message);
-    } else if (typeof error === 'string') {
-      core.setFailed(error);
-    } else {
-      core.setFailed('Unknown error! Check the logs for more information.');
-    }
-  }
+      })
+    .catch((error: Error) => actionsConsole.fail(`Unable to check staged files: ${error}`));
+  });
 }
 
 run();
