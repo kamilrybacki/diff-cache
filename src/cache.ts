46ccba9fc1456a0741b29369183597aa60ec7b4a
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-09 15:15:30 +0100

    try downloading all artifacts

--- src/cache.ts
+++ src/cache.ts
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
 import fs from 'fs/promises';
+import {
+  create as createArtifactClient,
+  ArtifactClient,
+  DownloadResponse
+} from '@actions/artifact';
 import { getOctokit, context } from '@actions/github';
 import { SimpleCrypto } from "simple-crypto-js"
 import LZString from 'lz-string';
@@ -7,13 +12,16 @@ import LZString from 'lz-string';
 import { GitHub } from '@actions/github/lib/utils';
 import { CommitComparisonResponse } from './types.js';
 
+
 class SimpleCache {
   repoPublicKey: string;
   repoPublicKeyId: string;
+  artifactClient: ArtifactClient;
   authenticatedAPI: InstanceType<typeof GitHub>;
-  encryptor: SimpleCrypto;
   source: string;
   target: string;
+  encrypt: (input: string) => string;
+  decrypt: (input: string) => string
   private static __instance: SimpleCache | undefined = undefined;
 
   constructor(
@@ -26,9 +34,12 @@ class SimpleCache {
     this.authenticatedAPI = authenticatedAPI;
     this.repoPublicKey = repoPublicKey;
     this.repoPublicKeyId = repoPublicKeyId;
-    this.encryptor = new SimpleCrypto(repoPublicKey);
+    this.artifactClient = createArtifactClient();
     this.source = source;
     this.target = target;
+    const encryptor: SimpleCrypto = new SimpleCrypto(repoPublicKey);
+    this.encrypt = (input: string) => encryptor.encrypt(input) as string;
+    this.decrypt = (input: string) => encryptor.decrypt(input) as string;
   }
 
   static access = async function (token: string): Promise<SimpleCache> {
@@ -109,25 +120,44 @@ class SimpleCache {
   };
 
   save = async function (this: SimpleCache, tag: string, value: string): Promise<void> {
-    const encryptedValue: string = this.encryptor.encrypt(value) as string;
+    const encryptedValue = this.encrypt(value);
     const compressedValue = LZString.compress(encryptedValue);
     await fs.writeFile(`${tag}`, compressedValue, 'utf-8')
-      .catch(() => {
-        throw new Error(`Unable to write artifact under path: ${tag}`);
-      })
-      .then(() => {
-        core.setOutput('files', value);
-        core.info(`Cached value for ${tag}: ${value}`);
-      });
+      .then(() => core.info(`Cached value for ${tag}: ${value}`))
+      .then(async () => await this.artifactClient.uploadArtifact(tag, [tag], '.')
+        .then(() => {
+          core.setOutput('files', value)
+          core.info(`Uploaded artifact for ${tag}`)
+        })
+        .catch((error) => {
+          throw new Error(`Unable to cache ${tag}: ${error}`);
+        })
+      );
   };
 
   load = async function (this: SimpleCache, tag: string): Promise<string> {
-    return await fs.readFile(tag, 'utf-8')
-      .catch(() => {
-        throw new Error(`Unable to open artifact under path: ${tag}`);
+    this.artifactClient.downloadAllArtifacts()
+    .then((response: DownloadResponse[]) => {
+      response.forEach((artifact: DownloadResponse) => {
+        core.info(`Downloaded artifact ${artifact.artifactName} to ${artifact.downloadPath}`);
       })
-      .then((encryptedValue: string) => this.encryptor.decrypt(encryptedValue) as string)
-      .then((value: string) => LZString.decompress(value) as string);
+    })
+    return await this.artifactClient.downloadArtifact(tag)
+      .then(async ({downloadPath}: {downloadPath: string}) => {
+        core.info(`Downloaded artifact to ${downloadPath}`);
+        return await fs.readFile(downloadPath, 'utf-8')
+          .catch(() => {
+            throw new Error(`Unable to open artifact under path: ${downloadPath}`);
+          })
+          .then((encryptedValue: string) => this.decrypt(encryptedValue) as string)
+          .then((value: string) => {
+            core.info(`Cached value for ${tag}: ${value}`);
+            return LZString.decompress(value) as string;
+          });
+      })
+      .catch(() => {
+        throw new Error(`Unable to download artifact: ${tag}`);
+      })
   };
 }
 
