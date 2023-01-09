bf52a998ddb75a4fc4749b6b0825efab949e58fb
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-09 14:56:26 +0100

    try composite action

--- src/cache.ts
+++ src/cache.ts
@@ -1,9 +1,5 @@
 import * as core from '@actions/core';
 import fs from 'fs/promises';
-import {
-  create as createArtifactClient,
-  ArtifactClient
-} from '@actions/artifact';
 import { getOctokit, context } from '@actions/github';
 import { SimpleCrypto } from "simple-crypto-js"
 import LZString from 'lz-string';
@@ -11,11 +7,9 @@ import LZString from 'lz-string';
 import { GitHub } from '@actions/github/lib/utils';
 import { CommitComparisonResponse } from './types.js';
 
-
 class SimpleCache {
   repoPublicKey: string;
   repoPublicKeyId: string;
-  artifactClient: ArtifactClient;
   authenticatedAPI: InstanceType<typeof GitHub>;
   encryptor: SimpleCrypto;
   source: string;
@@ -32,7 +26,6 @@ class SimpleCache {
     this.authenticatedAPI = authenticatedAPI;
     this.repoPublicKey = repoPublicKey;
     this.repoPublicKeyId = repoPublicKeyId;
-    this.artifactClient = createArtifactClient();
     this.encryptor = new SimpleCrypto(repoPublicKey);
     this.source = source;
     this.target = target;
@@ -116,43 +109,25 @@ class SimpleCache {
   };
 
   save = async function (this: SimpleCache, tag: string, value: string): Promise<void> {
-    const encryptedValue = this.encrypt(value);
+    const encryptedValue: string = this.encryptor.encrypt(value) as string;
     const compressedValue = LZString.compress(encryptedValue);
     await fs.writeFile(`${tag}`, compressedValue, 'utf-8')
-      .then(() => core.info(`Cached value for ${tag}: ${value}`))
-      .then(async () => await this.artifactClient.uploadArtifact(tag, [tag], '.')
-        .then(() => core.info(`Uploaded artifact for ${tag}`)).then(() => true))
-        .catch((error) => {
-          throw new Error(`Unable to cache ${tag}: ${error}`);
-        }
-      );
+      .catch(() => {
+        throw new Error(`Unable to write artifact under path: ${tag}`);
+      })
+      .then(() => {
+        core.setOutput('files', value);
+        core.info(`Cached value for ${tag}: ${value}`);
+      });
   };
 
   load = async function (this: SimpleCache, tag: string): Promise<string> {
-    return await this.artifactClient.downloadArtifact(tag)
-      .then(async ({downloadPath}: {downloadPath: string}) => {
-        core.info(`Downloaded artifact to ${downloadPath}`);
-        return await fs.readFile(downloadPath, 'utf-8')
-          .catch(() => {
-            throw new Error(`Unable to open artifact under path: ${downloadPath}`);
-          })
-          .then((encryptedValue: string) => this.decrypt(encryptedValue))
-          .then((value: string) => {
-            core.info(`Cached value for ${tag}: ${value}`);
-            return LZString.decompress(value) as string;
-          });
-      })
+    return await fs.readFile(tag, 'utf-8')
       .catch(() => {
-        throw new Error(`Unable to download artifact: ${tag}`);
+        throw new Error(`Unable to open artifact under path: ${tag}`);
       })
-  };
-
-  encrypt = function (this: SimpleCache, message: string): string {
-    return this.encryptor.encrypt(message) as string;
-  };
-
-  decrypt = function (this: SimpleCache, encrypted: string): string {
-    return this.encryptor.decrypt(encrypted) as string;
+      .then((encryptedValue: string) => this.encryptor.decrypt(encryptedValue) as string)
+      .then((value: string) => LZString.decompress(value) as string);
   };
 }
 
