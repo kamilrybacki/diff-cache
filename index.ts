4c5a14b048364eeb8551a095057ae5c10e9f71ee
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-18 17:04:16 +0100

    make it better

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ async function run() {
           core.info(`Cached files: ${cachedFilesList}`);
           core.info(`Staged files: ${stagedFilesList}`);
           if (stagedFilesList.length) {
-            const filesToCache = stagedFilesList.filter((file: string) => !cachedFilesList.includes(file));
+            const filesToCache = [...new Set(...stagedFiles, ...cachedFilesList)];
             if (filesToCache.length && filesToCache !== cachedFilesList) {
               core.info(`Files to cache: ${filesToCache}`);
               await cache.save(cacheTag, filesToCache.join(' '));
