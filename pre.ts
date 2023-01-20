7064dd4535252d3c52d184c960811981253ce85b
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-20 16:41:03 +0100

    test 2

--- pre.ts
+++ pre.ts
@@ -4,13 +4,13 @@ import {getOctokit, context} from '@actions/github';
 export const prerun = async () => {
   const token = core.getInput('token', {required: true});
   const api = getOctokit(token);
-  await api.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs?sort=completed_at:desc', {
+  await api.request('GET /repos/{owner}/{repo}/commits/{head_sha}', {
     owner: context.repo.owner,
     repo: context.repo.repo,
-    run_id: context.runId
-  })
-    .catch((error: Error) => {
-      throw new Error(`Unable to get workflow info: ${error.message}`);
-    })
-    .then((response) => core.info(`Workflow info: ${JSON.stringify(response.data)}`))
+    head_sha: context.sha,
+  }).then(
+    (response) => {
+      core.info(`Response: ${JSON.stringify(response)}`);
+    }
+  )
 };
