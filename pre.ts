03e039d75a505ec758218576e61380fe07d2596c
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-20 16:34:27 +0100

    yupo

--- pre.ts
+++ pre.ts
@@ -4,6 +4,5 @@ import {getOctokit, context} from '@actions/github';
 export const prerun = async () => {
   const token = core.getInput('token', {required: true});
   const api = getOctokit(token);
-  core.info(JSON.stringify(context)
-
+  core.info(JSON.stringify(context));
 };
