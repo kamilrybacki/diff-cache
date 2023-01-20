f3a30f188260b1bce201b97b68439fcb647f75c4
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-20 16:59:51 +0100

    ?

--- pre.ts
+++ pre.ts
@@ -5,21 +5,17 @@ import { request } from 'http';
 export const prerun = async () => {
   const token = core.getInput('token', {required: true});
   const api = getOctokit(token);
-  await api.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
+  await api.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
     owner: context.repo.owner,
     repo: context.repo.repo,
-    workflow_id: context.workflow,
-  })
-    .then(({data}) => {
-      core.info(JSON.stringify(data, null, 2));
-      // const currentWorkflowUrl = data.url;
-      // core.info(`Current workflow url: ${currentWorkflowUrl}`);
-      // return request(currentWorkflowUrl);
-    })
-    // .catch((error: Error) => {
-    //   throw new Error(`Unable to get current workflow: ${error.message}`);
-    // })
-    // .then(({path}) => {
-    //   core.info(`Current workflow file: ${path}`);
-    // });
+    run_id: context.runId,
+  }).then(({data}) => {
+    const currentWorkflowUrl = data.workflow_url;
+    core.info(`Current workflow url: ${currentWorkflowUrl}`);
+    return request(currentWorkflowUrl, (res) => {
+      res.on('data', (chunk) => {
+        core.info(`Current workflow: ${chunk}`);
+      });
+    });
+  });
 };
