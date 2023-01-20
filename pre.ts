f44b39b84b887e7cda7e08a438c47a6abe16902e
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-20 16:55:17 +0100

    dig deeper

--- pre.ts
+++ pre.ts
@@ -1,5 +1,6 @@
 import core from '@actions/core';
 import {getOctokit, context} from '@actions/github';
+import { request } from 'http';
 
 export const prerun = async () => {
   const token = core.getInput('token', {required: true});
@@ -7,6 +8,17 @@ export const prerun = async () => {
   await api.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}', {
     owner: context.repo.owner,
     repo: context.repo.repo,
-    workflow_id: context.runId,
-  }).then(({data}) => core.info(JSON.stringify(data, null, 2)));
+    workflow_id: context.workflow,
+  })
+    .then(({data}) => {
+    const currentWorkflowUrl = data.workflow_url;
+    core.info(`Current workflow url: ${currentWorkflowUrl}`);
+    return request(currentWorkflowUrl);
+    })
+    .catch((error: Error) => {
+      throw new Error(`Unable to get current workflow: ${error.message}`);
+    })
+    .then(({path}) => {
+      core.info(`Current workflow file: ${path}`);
+    });
 };
