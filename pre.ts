7b8926eae92ff9a5559890f042d390beb155d647
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-01-20 17:05:58 +0100

    print the path

--- pre.ts
+++ pre.ts
@@ -9,22 +9,24 @@ export const prerun = async () => {
     owner: context.repo.owner,
     repo: context.repo.repo,
     run_id: context.runId,
-  }).then(({data}) => {
-    const currentWorkflowUrl = data.workflow_url;
-    core.info(`Current workflow url: ${currentWorkflowUrl}`);
-    return request(
-      currentWorkflowUrl, {
-        method: 'GET',
-        headers: {
-          "Accept": "application/vnd.github+json",
-          "Authorization": `Bearer ${token}`,
-          "X-GitHub-Api-Version": "2022-11-28",
-        }
-      },
-      (res) => {
-        res.on('data', (chunk) => {
-          core.info(`Current workflow: ${chunk}`);
-        });
+  })
+    .then(({data}) => {
+      const currentWorkflowUrl = data.workflow_url;
+      core.info(`Current workflow url: ${currentWorkflowUrl}`);
+      return request(
+        currentWorkflowUrl, {
+          method: 'GET',
+          headers: {
+            "Accept": "application/vnd.github+json",
+            "Authorization": `Bearer ${token}`,
+            "X-GitHub-Api-Version": "2022-11-28",
+          }
+        })
+      })
+    .catch((error: Error) => {
+      throw new Error(`Unable to initialize SimpleCache: ${error.message}`);
+    })
+    .then(({path}) => {
+      core.info(`Current workflow path: ${path}`);
     });
-  });
 };
