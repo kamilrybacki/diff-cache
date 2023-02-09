0356e94104c48b4d2a983edc97ab1a2c67e52101
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2023-02-09 13:59:50 +0100

    print commit files

--- src/cache.ts
+++ src/cache.ts
@@ -232,6 +232,7 @@ class DiffCache {
 
   removeFilesNotPresentInCurrentCommit = async function (this: DiffCache, files: string[]): Promise<string[]> {
     const currentCommitFiles = await this.getCurrentCommitFilesList();
+    console.log(currentCommitFiles)
     return files.filter((file: string) => currentCommitFiles.includes(file));
   };
 
